8df260d0b93b5c928a2998d91b481c0864a19e39
songhu <305123882@qq.com>
2018-03-22 14:13:13 +0800

    新建demo

--- SpringBoot-helloWorld/src/main/java/com/somta/springboot/Application.java
+++ SpringBoot-helloWorld/src/main/java/com/somta/springboot/Application.java
@@ -1,14 +1,13 @@
 package com.somta.springboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String [] args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
-
+		SpringApplication.run(Application.class, args);
     }
 }
