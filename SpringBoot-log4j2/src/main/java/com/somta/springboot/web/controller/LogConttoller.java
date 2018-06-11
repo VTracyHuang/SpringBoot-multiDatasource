d745696810bf7fcd369313fc4c79f947cec8f8f3
明天的地平线 <305123882@qq.com>
2018-06-11 18:05:00 +0800

    修改配置

--- SpringBoot-log4j2/src/main/java/com/somta/springboot/web/controller/LogConttoller.java
+++ SpringBoot-log4j2/src/main/java/com/somta/springboot/web/controller/LogConttoller.java
@@ -1,7 +1,7 @@
 package com.somta.springboot.web.controller;
 
-import org.slf4j.Logger;  
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pub")
 public class LogConttoller {
 	
-	private final static Logger logger = LoggerFactory.getLogger(LogConttoller.class);
+	private static final Logger logger = LogManager.getLogger(LogConttoller.class);
    
 	@GetMapping("/getLog")
 	public String getLog(){  
